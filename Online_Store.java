69b91c5de8a8b158c85b27ff0642c67be0436350
unknown <winnie.joe123@gmail.com>
2020-01-10 08:26:45 -0700

    initialize Online_Store

---
+++ Online_Store.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Online_Store{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome to the online store!");
+
+		System.out.println("What is your name?");
+		String name = scan.nextLine();
+		System.out.println("What is your email?");
+		String email = scan.nextLine();
+		Customer name = new Customer(name, email);
+		
+		
+
+	}
